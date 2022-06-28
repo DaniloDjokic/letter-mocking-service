03b7884fccf4e2a3ab892c97fedd5e5aaac6382b
Danilo Djokic <danilo.djokic@fincore.com>
2022-06-28 16:00:13 +0200

    Add notification icon

--- Mocking.py
+++ Mocking.py
@@ -2,18 +2,26 @@ import keyboard, mocking_flow as mf
 import configuration_handler as ch
 from global_hotkeys import *
 from win10toast import ToastNotifier
+from pathlib import Path
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("./assets")
 
 mocking_flow = mf.MockingFlow()
 configuration_handler = ch.ConfigurationHandler()
 
 print("App started")
 
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
 def displayNotification(toast, text):
-     toast.show_toast(
+    toast.show_toast(
         "Keyboard",
         text,
+        icon_path=relative_to_assets('../assets/icon.ico'),
         duration = 1.5,
-        threaded = True
+        threaded = True,
     )
 
 def onHotkey():
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 import py2exe 
 
-setup(console=['Mocking.pyw'])
+setup(console=['Mocking.py'])
